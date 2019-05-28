8c900a0401033be8dfdce96a06e2068d5ca472a7
hoonnn <hoon8675@gmail.com>
2019-05-28 22:55:24 +0900

    intent 2space 수정

--- vue-docker-registry/src/components/HelloWorld.vue
+++ vue-docker-registry/src/components/HelloWorld.vue
@@ -8,8 +8,10 @@
     </p>
     <h3>Installed CLI Plugins</h3>
     <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
+      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank"
+             rel="noopener">babel</a></li>
+      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank"
+             rel="noopener">eslint</a></li>
     </ul>
     <h3>Essential Links</h3>
     <ul>
@@ -23,7 +25,8 @@
     <ul>
       <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
       <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
+      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a>
+      </li>
       <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
       <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
     </ul>
@@ -31,47 +34,50 @@
 </template>
 
 <script>
-    // const axios = require('axios');
-    // axios.get({
-    //     url: 'http://localhost:5000/v2/_catalog',
-    //     auth: {
-    //         username: 't',
-    //         password: 't'
-    //     }
-    // })
-    //     .then(function (response) {
-    //         // handle success
-    //         console.log(response);
-    //     })
-    //     .catch(function (error) {
-    //         // handle error
-    //         console.log(1 + error);
-    //     })
-    //     .finally(function () {
-    //         console.log("finally")
-    //     });
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
+  // const axios = require('axios');
+  // axios.get({
+  //     url: 'http://localhost:5000/v2/_catalog',
+  //     auth: {
+  //         username: 't',
+  //         password: 't'
+  //     }
+  // })
+  //     .then(function (response) {
+  //         // handle success
+  //         console.log(response);
+  //     })
+  //     .catch(function (error) {
+  //         // handle error
+  //         console.log(1 + error);
+  //     })
+  //     .finally(function () {
+  //         console.log("finally")
+  //     });
+  export default {
+    name: 'HelloWorld',
+    props: {
+      msg: String
+    }
   }
-}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+  h3 {
+    margin: 40px 0 0;
+  }
+  
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+  
+  li {
+    display: inline-block;
+    margin: 0 10px;
+  }
+  
+  a {
+    color: #42b983;
+  }
 </style>
